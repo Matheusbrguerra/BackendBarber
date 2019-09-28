ab62d903033b3637ccc158bbd6a83ff6b7728cf3
Matheusbrguerra <matheusbrguerra@hotmail.com>
2019-09-28 20:10:31 -0300

    Appointment function delete has been created and notification update too, the feature who send emails for each canceled appointment has start to create, express handlebars and nodemailer-hbs dependency has added and already to keep configuring config/mail his directory lib/Mail include Queues with Redis DB

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -13,11 +13,24 @@ class NotificationController {
         .json({ error: "Only provider can load notifiations" });
     }
 
-    const notifications = await Notification.find({ user: req.userId })
+    const notifications = await Notification.find({
+      user: req.userId
+    })
       .sort({ createdAt: "desc" })
       .limit(20);
 
     return res.json(notifications);
+  }
+  async update(req, res) {
+    // const notification = await Notification.findById(req.params.id);
+
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
   }
 }
 
